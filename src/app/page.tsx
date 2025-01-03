4c34628a90bd8f82368203ec88a4c19dc079c386
Dan Rasmuson <danrasmuson@gmail.com>
2025-01-03 15:55:39 -0600

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,33 +3,46 @@ import { createEmbeddings } from "@/utils-server/openai/create-embedding";
 import styles from "./page.module.css";
 import { PlotExample } from "./plot.tsx";
 
+async function addCoordinatesToWords(words: string[]): { text: string, x: number, y: number, z: number }[] {
+  const embeddings = await createEmbeddings(words, { dimensions: 3 })
+  return embeddings.map(({ embedding }, index) => {
+    const [x, y, z] = embedding;
+    return {
+      text: words[index],
+      x,
+      y,
+      z
+    }
+  })
+}
+
+function addColor<T>(word: T) {
+  return {
+    ...word,
+    color: 'red'
+  }
+}
+
 export default async function Home() {
   const [{ embedding: catEmbeddingFull }] = await createEmbeddings(['cat'])
   const [{ embedding: catEmbeddingThree }] = await createEmbeddings(['cat'], { dimensions: 3 })
-  const [{ embedding: catEmbedding }, { embedding: dogEmbedding }, { embedding: baseballEmbedding }] = await createEmbeddings(['cat', 'cats', 'baseball'], { dimensions: 3 })
+  const words = [
+    "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew",
+    "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry",
+    "strawberry", "tangerine", "ugli", "vanilla", "watermelon", "xigua", "yam", "zucchini",
+    "airplane", "balloon", "cat", "dog", "elephant", "frog", "giraffe", "hat", "igloo",
+    "jellyfish", "kite", "lamp", "moon", "notebook", "ocean", "pencil", "queen", "robot",
+    "sun", "tree", "umbrella", "vase", "window", "xylophone", "yacht", "zebra", "ant",
+    "bear", "cloud", "dolphin", "engine", "feather", "garden", "house", "island",
+    "jacket", "kangaroo", "ladder", "mountain", "nest", "octopus", "penguin", "quilt",
+    "ring", "ship", "turtle", "unicorn", "violin", "whale", "xenon", "yogurt", "zipper",
+    "acorn", "bottle", "camera", "desk", "egg", "forest", "globe", "horizon", "iceberg",
+    "jungle", "key", "lantern", "mirror", "nectar", "owl", "peach", "quasar", "rainbow"
+  ];
 
-  const points = [
-    {
-      text: 'cat',
-      color: 'red',
-      x: catEmbedding[0],
-      y: catEmbedding[1],
-      z: catEmbedding[2],
-    },
-    {
-      text: 'cats',
-      color: 'green',
-      x: dogEmbedding[0],
-      y: dogEmbedding[1],
-      z: dogEmbedding[2],
-    }, {
-      text: 'baseball',
-      color: 'orange',
-      x: baseballEmbedding[0],
-      y: baseballEmbedding[1],
-      z: baseballEmbedding[2],
-    }
-  ]
+  const [{ embedding: catEmbedding }, { embedding: dogEmbedding }, { embedding: baseballEmbedding }] = await createEmbeddings(words, { dimensions: 3 })
+  const wordsWithEmbeddings = await addCoordinatesToWords(words)
+  const points = wordsWithEmbeddings.map(addColor)
 
   return (
     <div>
@@ -43,9 +56,6 @@ export default async function Home() {
       <br />
       <br />
 
-      <div>cat: {JSON.stringify(catEmbedding)}</div>
-      <div>dog: {JSON.stringify(dogEmbedding)}</div>
-      <div>baseballEmbedding: {JSON.stringify(baseballEmbedding)}</div>
       <PlotExample points={points} />
 
 
