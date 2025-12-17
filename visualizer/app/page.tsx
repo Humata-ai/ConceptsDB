902771e855296106254928a80f8f790dff6f6786
Dan Rasmuson <danrasmuson@gmail.com>
2025-12-17 09:23:49 -0600

    Reset visualizer directory to commit 23014bd state
    Reverting visualizer changes after 23014bd to restore basic visualization state

--- visualizer/app/page.tsx
+++ visualizer/app/page.tsx
@@ -1,31 +1,11 @@
-'use client';
-
-import { useState } from 'react';
 import ConceptualSpace from './components/ConceptualSpace';
-import ColorInfoHUD from './components/ColorInfoHUD';
-import spaceConfig from './config/conceptual-space.json';
-import { LABColor, RGBColor } from './utils/colorConversion';
 
 export default function Home() {
-  const [selectedColor, setSelectedColor] = useState<{
-    lab: LABColor;
-    rgb: RGBColor;
-  } | null>(null);
-
-  const handleColorSelect = (lab: LABColor, rgb: RGBColor) => {
-    setSelectedColor({ lab, rgb });
-  };
-
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
-      <ConceptualSpace
-        xRange={spaceConfig.xRange as [number, number]}
-        yRange={spaceConfig.yRange as [number, number]}
-        zRange={spaceConfig.zRange as [number, number]}
-        samplingRate={spaceConfig.samplingRate}
-        onColorSelect={handleColorSelect}
-      />
-      <ColorInfoHUD selectedColor={selectedColor} />
-    </div>
+    <ConceptualSpace
+      xRange={[-10, 10]}
+      yRange={[-10, 10]}
+      zRange={[-10, 10]}
+    />
   );
 }
